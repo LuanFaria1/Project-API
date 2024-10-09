431ea208aefec463e9b9cac6ddd2ad0ef84d60b9
LuanFaria1 <farialuan723@gmail.com>
2024-10-09 17:42:33 -0300

    Criação DataBase

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Inicialize o SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     
-    # Configuração do banco de dados
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Carregue as configurações do banco de dados
+    app.config.from_object('app.config.Config')
     
+    # Inicialize o banco de dados com a aplicação Flask
     db.init_app(app)
-
-    # Registrar rotas
+    
+    # Registrar as rotas
     from .routes import main
     app.register_blueprint(main)
     
